d77a87504a4fac1dd4675791e3aacf1785f9478f
Eugene Kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-12 21:36:15 +0300

    Вызов Page View Controller, в момент загрузки View Controller. Отображение Page View Controller.

--- PresentationMe/PageViewController.swift
+++ PresentationMe/PageViewController.swift
@@ -10,20 +10,38 @@ import UIKit
 
 class PageViewController: UIPageViewController {
 
+    let presentScreenContent = ["hello, my name is Eugene",
+                                "i'm a iOS-developer",
+                                "every day i study in this direction",
+                                "therefore I am open to interesting projects"]
+    let emojiArray = ["✋🏽",
+                     "🦾",
+                     "👨🏽‍💻",
+                     "🤷🏽"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let contentViewController = showViewControllerAtIndex(0) {
+            setViewControllers([contentViewController],
+                               direction: .forward,
+                               animated: true)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
+        
+        guard index >= 0 && index < presentScreenContent.count,
+            let contentViewController = storyboard?.instantiateViewController(
+                withIdentifier: "ContentViewController") as? ContentViewController
+            else { return nil }
+        
+        contentViewController.presentText = presentScreenContent[index]
+        contentViewController.emoji = emojiArray[index]
+        contentViewController.currentPage = index
+        contentViewController.numberOfPages = presentScreenContent.count
+        
+        return contentViewController
     }
-    */
 
 }
--- PresentationMe/ViewController.swift
+++ PresentationMe/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        startPresentation()
     }
 
-
+    func startPresentation() {
+        
+        if let pageViewController = storyboard?.instantiateViewController(
+            withIdentifier: "PageViewController") as? PageViewController {
+            present(pageViewController, animated: true)
+        }
+    }
 }
 
