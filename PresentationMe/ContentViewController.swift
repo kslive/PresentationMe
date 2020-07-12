e17472b04c1c6626f1728c0e6b6b798c51af85a0
Eugene Kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-12 20:05:01 +0300

    Interface setup.

---
+++ PresentationMe/ContentViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ContentViewController.swift
+//  PresentationMe
+//
+//  Created by Eugene Kiselev on 12.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    @IBOutlet weak var presentTextLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PresentationMe/PageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageViewController.swift
+//  PresentationMe
+//
+//  Created by Eugene Kiselev on 12.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
